6c5e02f6fb29db3d1b230296218c39c09e394660
MikhailPtichkin88 <mikhailptichkin1988@gmail.com>
2022-06-30 13:03:17 +0300

    Updates

---
+++ precache-manifest.5192f61535655071e7e92fcf495e7e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c1c22584bbeaee77de35f75d7c890e",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "8d7d2ee08d2b6830ea45",
+    "url": "/TodoList/static/css/main.2350973d.chunk.css"
+  },
+  {
+    "revision": "dbcf1524802ae4976c22",
+    "url": "/TodoList/static/js/2.c09889db.chunk.js"
+  },
+  {
+    "revision": "01261462acf6f67ce7f7ef9e8e890a26",
+    "url": "/TodoList/static/js/2.c09889db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7d2ee08d2b6830ea45",
+    "url": "/TodoList/static/js/main.36cd685e.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
