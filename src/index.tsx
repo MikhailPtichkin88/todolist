bc9172848287337a9ee8c32889974d4b93862074
Mikhail <mikhailptichkin1988@gmail.com>
2022-10-24 00:03:32 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import './index.css';
 import {Provider} from "react-redux";
 import {store} from "./app/store";
 import App from "./app/App";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
         <App />
     </Provider>
-    </BrowserRouter>
+    </HashRouter>
     ,  document.getElementById('root'));
